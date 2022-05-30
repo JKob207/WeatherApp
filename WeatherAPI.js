c7febdd4ca9c08423f3041e237dbea7a12e90d8d
JKob207 <44620136+JKob207@users.noreply.github.com>
2022-05-30 16:33:13 +0200

    Update WeatherAPI.js

--- WeatherAPI.js
+++ WeatherAPI.js
@@ -1,7 +1,7 @@
 export const getDataByCity = (city) =>
 {
     const API_KEY  = "7cd5ca586d684ea09e8140909222805";
-    const URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7&aqi=no&alerts=no`;
+    const URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7&aqi=no&alerts=no`;
 
     return fetch(URL)
     .then(res => res.json())
